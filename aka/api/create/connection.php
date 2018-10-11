652b48dbedeec82291e89dfbd65fb2579893db25
Seia Soto <seriumium@outlook.kr>
2018-10-12 02:25:37 +0900

    Add uri handler to gateway

--- aka/api/create/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(dirname(__FILE__) . '../get/configurations.php');
-
-$link = new MySQLi($database['host'], $database['user'], $database['password'], $database['name']);
-if ($link -> connect_errno) {
-  die('Connection Error: ' . $link -> connect_error);
-}
- ?>
